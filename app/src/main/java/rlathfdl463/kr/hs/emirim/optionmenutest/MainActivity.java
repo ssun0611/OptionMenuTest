cbd04c43dead69eca4e5bc020c406462c772ee9c
ssun0611 <rlathfdl463@naver.com>
2017-05-22 20:49:52 +0900

    #2 onCreateOptionsMenu

--- app/src/main/java/rlathfdl463/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -2,6 +2,8 @@ package rlathfdl463.kr.hs.emirim.optionmenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super. onCreateOptionsMenu(menu);
+        MenuInflater mInflater= getMenuInflater();
+        mInflater.inflate(R.menu.menu1,menu); //두번째 메뉴는 위에 쓴 menu
+        return true;
     }
 }
